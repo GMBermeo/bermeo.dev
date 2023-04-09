bb92219095753c96193ec8b5de023244ee9891dc
Guilherme Bermêo <gm@bermeo.dev>
2023-04-09 20:45:38 -0300

    ✨ MD file reading and slug testing for StaticProps

---
+++ _data/banco-do-brasil-br.md
@@ -0,0 +1,55 @@
+# UX Designer
+
+BB Tecnologia e Serviços
+Abr 2015 - Set 2018
+
+## Software de Gestão Empresarial
+
+![BB Plugins Mockup](https://bermeo.dev/images/bbts/PLUGINS-Group-2160.png)
+Conceitualizei uma solução de software de gestão inovadora na Diretoria de Novos Produtos de Negócios do Banco do Brasil. O software inovador foi projetado para substituir o Gestão BB, simplificando pagamentos, recebimentos e outros serviços para o setor contábil dos clientes jurídicos do Banco do Brasil. O software priorizava a navegação apenas com o teclado, apresentando um protótipo de alta fidelidade no Axure RP com 47 telas e formulários dinâmicos. Testado internamente por seis meses, o software visava oferecer aos usuários uma experiência eficiente e contínua.
+
+---
+
+## Aplicativo BB Mobile versão 3
+
+![BB Mobile App 3 - Material de Marketing](https://bermeo.dev/images/bbts/APP-BB-3_1.jpg)
+![BB Mobile App 3 - Material de Marketing](https://bermeo.dev/images/bbts/APP-BB-3_2.jpg)
+![BB Mobile App 3 - Material de Marketing](https://bermeo.dev/images/bbts/APP-BB-3_3.jpg)
+Colaborei no design, prototipagem e teste de diversas funcionalidades durante a migração do Aplicativo Mobile 2.0 para 3.0. Utilizando a arquitetura React Native, o aplicativo eliminou a necessidade de componentes nativos, proporcionando maior liberdade criativa e consistência entre plataformas. Com ampla pesquisa de usuário, teste e design iterativo, o projeto de dois anos permitiu o desenvolvimento simultâneo de vários componentes, resultando em um aplicativo altamente funcional e fácil de usar.
+
+---
+
+## SINDEC
+
+Desenvolvi o front-end para o Sistema Nacional de Informações de Defesa do Consumidor, um software interno utilizado pelo PROCON (Programa de Proteção e Defesa do Consumidor) em todo o Brasil. Ao seguir as diretrizes do Google Material Design e Bootstrap, o projeto garantiu uma identidade visual coesa e moderna para a organização.
+
+---
+
+## Consumidor.gov.br
+
+![Consumidor.gov.br Mobile App](https://bermeo.dev/images/bbts/APP-CONSUMIDOR-2160.jpg)
+Reformulei o portal de proteção ao consumidor (consumidor.gov.br) com prototipagem de alta fidelidade no Axure RP e design de movimento. Realizei testes A/B com grupos internos para refinar a experiência do usuário e, posteriormente, desenvolvi o front-end do site.
+
+---
+
+## Gerenciamento de Projetos de Infraestrutura
+
+![GPI](https://bermeo.dev/images/bbts/GPI-1080.jpg)
+Criei um software de gerenciamento de ordens de serviço para uso interno da equipe de Projetos de Infraestrutura do Banco do Brasil. Em um período de concepção de três meses, o projeto envolveu desenvolvimento de front-end usando Bootstrap, agilizando os processos de gerenciamento de projetos dentro da organização.
+
+---
+
+## Aplicativo BB Mobile versão 2
+
+![BB Mobile App 2](https://bermeo.dev/images/bbts/APP-TRANSFERENCIA.jpg)
+![BB Mobile App 2 - Windows Phone](https://bermeo.dev/images/bbts/APP-BB_Windows-1080.jpg)
+Liderei a prototipagem de alta fidelidade no Axure RP para o "novo" aplicativo móvel do Banco do Brasil. Adaptei o aplicativo existente de 2011 (1.0) para a versão 2.0 com uma aparência renovada. Gerenciei o desenvolvimento e teste de novos recursos, acomodando as restrições exclusivas das plataformas Android, iOS e Windows Phone. O aplicativo atualizado, lançado em 2014, passou de uma arquitetura iframe para arquiteturas de componentes nativos (swift, java e c#, de acordo com as plataformas móveis), aprimorando a experiência do usuário em dispositivos.
+
+---
+
+## Outros Projetos
+
+![BB Hotsite](https://bermeo.dev/images/bbts/HOTSITE-2160.jpg)
+![BB Intranet Modular](https://bermeo.dev/images/bbts/INTRANET-MODULAR-2160.jpg)
+![BB Intranet Modular Mobile](https://bermeo.dev/images/bbts/INTRANET-MODULAR-MOBILE.jpg)
+![BB Mobile App 2](https://bermeo.dev/images/bbts/APP-LIMITES.jpg)
--- pages/banco-do-brasil.tsx
+++ pages/banco-do-brasil.tsx
@@ -4,8 +4,27 @@ import { Subtitle } from "../components/ui/Subtitle";
 import Head from "next/head";
 import { useContext } from "react";
 import { LocaleContext } from "@contexts/LocaleContext";
+import fs from "fs";
+import matter from "gray-matter";
+import ReactMarkdown from "react-markdown";
+import s from "styles/markdown.module.css";
+import { NavBar } from "@components/NavBar/NavBar";
 
-const BB: NextPage = () => {
+export async function getStaticProps(context: any) {
+  const filename = fs.readFileSync(
+    `_data/banco-do-brasil-${context.locale}.md`,
+    "utf8"
+  );
+  const { content } = matter(filename);
+
+  return {
+    props: {
+      content,
+    },
+  };
+}
+
+const BB: NextPage<{ content: string }> = ({ content }) => {
   const { isEng } = useContext(LocaleContext);
 
   return (
@@ -13,8 +32,8 @@ const BB: NextPage = () => {
       <Head>
         <title>
           {isEng()
-            ? "Bank of Brazil Technology and Services images "
-            : "Imagens do Banco do Brasil Tecnologia & Serviços "}
+            ? "Bank of Brazil Technology and Services"
+            : "Banco do Brasil Tecnologia & Serviços"}
           - Guilherme Bermeo
         </title>
 
@@ -22,83 +41,16 @@ const BB: NextPage = () => {
           name="description"
           content={
             isEng()
-              ? "Bank of Brazil Technology and Services images "
-              : "Imagens do Banco do Brasil Tecnologia & Serviços "
+              ? "Bank of Brazil Technology and Services"
+              : "Banco do Brasil Tecnologia & Serviços"
           }
         />
       </Head>
-      <main className="container mx-auto max-w-3xl px-6 sm:px-8">
+      <NavBar />
+      <main className="container mx-auto mt-7 max-w-3xl px-6 sm:mt-14 sm:px-8">
         <Title title={"Banco do Brasil"} />
-        <Subtitle title={"Software de Gestão Empresarial"} />
-        <article className="customContainer">
-          Software conceitualizado na Diretoria de Novos Produtos Empresariais
-          dentro do Banco do Brasil. Um software de gestão que viria a
-          substituir o Gestão BB, facilitando pagamentos, recebimento, e outros
-          serviços para o setor contábil de clientes jurídicos (empresas) do
-          Banco do Brasil. O projeto fora concebido e testado internamente
-          durante 6 meses, a ideia principal que rondava a utilização deste
-          software se dava no fato de todas as ações poderem ser feitas sem
-          necessitar a utilização do mouse. A inspiração para essa
-          funcionalidade de navegação via teclado veio da observação de
-          atendentes de clientes juridicos em que preferiam a utilização de um
-          sistema mais antigo, monocromático, em ordem da utilização apenas do
-          teclado e responsividade que não se obtia pelo software “novo” que
-          estava no mercado. A protipação de alta fidelidade em Axure RP contou
-          com 47 telas, com formulários dinâmicos e utilização 100% funcional
-          apenas por teclado, para futuras provas de conceito.
-        </article>
-        <Subtitle title={"App BB Mobile (versão 3)"} />
-        <article className="customContainer">
-          Atuação na concepção, desenho, prototipagem e teste de diversas
-          funcionalidades na migração do App Mobile 2.0 parta o 3.0, tendo em
-          mente a nova arquitetura React Native onde não se é mais necessário o
-          uso de componentes nativos de cada tecnologia (swift para iOS e java
-          para Android), possibilitando muito mais liberdade e união entre
-          versões. Com a nova biblioteca de componentes próprios, fomos capazes
-          de desenvolver uma tela de cartões de crédito com muita liberdade
-          criativa. Parte dos testes foi feito com pesquisa de usuário externo
-          em grupos de testes, grupos internos, método de Wizard of Oz e testes
-          A/B. O desenvolvimento da nova interface durou 2 anos, incluindo as
-          fases de concepção e testes, que graças à modularidade do app, nos
-          permitiu desenvolver diferentes partes ao mesmo tempo.
-        </article>
-        <Subtitle title={"SINDEC"} />
-        <article className="customContainer">
-          Desenvolvimento front-end do Sistema Nacional de Informações de Defesa
-          do Consumidor, software interno utilizado na gestão de atendimentos
-          realizados pelo PROCON (Programa de Proteção e Defesa do Consumidor)
-          em todos os estados do Brasil. O desenvolvimento dos componentes foi
-          baseado no Google Material Design guideline e nos style guidelines da
-          própria plataforma (Bootstrap) com adaptação para a nova identidade
-          visual do orgão.
-        </article>
-        <Subtitle title={"Consumidor.gov.br"} />
-        <article className="customContainer">
-          Novo portal de defesa do consumidor (consumidor.gov.br) com motion
-          design em prototipação de alta fidelidade em Axure RP, teste A/B com
-          grupos internos e posterior desenvolvimento front-end da página.
-        </article>
-        <Subtitle title={"Gestão de Projetos de Infraestrutura"} />
-        <article className="customContainer">
-          Software de uso interno requisitado pela equipe de Proketos de
-          Infraestrutura do Banco do Brasil, foi um gerenciador de ordens de
-          serviços que contou com um periodo de concepção de 3 meses, onde atuei
-          no desenvolvimento front-end utilizando bootstrap.
-        </article>
-        <Subtitle title={"App BB Mobile (versão 2)"} />
-        <article className="customContainer">
-          Prototipação de alta fidelidade em Axure RP do “novo” aplicativo do
-          Banco do Brasil, com o desenvolvimento e teste de novas
-          funcionalidades com teste em grupo de usuários internos. O grande
-          desafio da versão 2 do aplicativo Mobile do Banco do Brasil, era
-          existir 3 versões distintas para cada plataforma (Android, iOS e
-          Windows Phone), onde cada qual possuia suas próprias limitações, então
-          cada nova funcionalidade necessitava ser desenhada e testada no mínimo
-          3 vezes. Fui responsável por maior parte da adaptação do aplicativo de
-          2011 (1.0) para a versão 2.0 com novo visual publicado em 2014,
-          passando de uma arquitetura iframe para arquiteturas de componentes
-          nativos (swift, java e c#, conforme plataformas mobiles).
-        </article>
+
+        <ReactMarkdown className={s.markdown}>{content}</ReactMarkdown>
       </main>
     </>
   );
