feaa6ffd93a91a9cad44cc8189a96443c87d32ab
Guilherme Bermêo <guilherme.bermeo@agu.gov.br>
2025-04-28 14:57:38 -0300

    ⬆️ Updated Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -53,3 +53,33 @@ const HomePage = async () => {
 };
 
 export default HomePage;
+
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined;
+
+function once(fn: Function): OnceFn {
+  let once: boolean = false;
+
+  return function (...args) {
+    if (!once) {
+      return undefined;
+    } else {
+      once = true;
+      return fn(...args);
+    }
+  };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
